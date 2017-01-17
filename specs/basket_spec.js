2e864198a7685dfff26793f80aeebeb79c6d6378
crhilditch20 <crhilditch20@gmail.com>
2017-01-17 15:49:09 +0000

    change test for basket starts with no items

--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('basket', function(){
   it('basket starts with no items', function(){
-    assert.equal(0, basket.items.length);
+    assert.equal(0, basket.countItems());
   });
   it('basket starts with 0 value', function(){
     assert.equal(0, basket.total);
